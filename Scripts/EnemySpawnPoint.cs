094ad1fe9533819dfbdcbf1432e882d962cb8425
Dmitry <diman567tak@gmail.com>
2024-04-29 19:22:43 +0300

    Added borders and enemy spawn effect

--- Scripts/EnemySpawnPoint.cs
+++ Scripts/EnemySpawnPoint.cs
@@ -5,6 +5,22 @@ public partial class EnemySpawnPoint : Sprite2D
     [Export]
     public PackedScene EnemyScene { get; set; }
 
+    private Timer _timer;
+
+    public override void _Ready()
+    {
+        _timer = GetNode<Timer>("EnemySpawnTimer");
+    }
+
+    public override void _Process(double delta)
+    {
+        Modulate = new Color(
+            Modulate.R,
+            Modulate.G,
+            Modulate.B,
+            (float)(1 - _timer.TimeLeft / _timer.WaitTime));
+    }
+
     public void OnEnemyTimerTimeout()
 	{
         Enemy enemy = EnemyScene.Instantiate<Enemy>();
